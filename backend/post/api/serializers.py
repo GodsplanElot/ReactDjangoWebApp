b290b8cdd9684cbb273bc80367ae781c30b4d37d
Godsplan Elot <elotgodsplan2@gmail.com>
2025-09-17 06:48:46 +0100

    modify serializers.py views.py urls.py

--- backend/post/api/serializers.py
+++ backend/post/api/serializers.py
@@ -1,3 +1,6 @@
 from rest_framework.serializers import ModelSerializer
-
-class PostSerializers()
+from ..models import post
+class PostSerializers(ModelSerializer):
+    class Meta:
+        model = post
+        fields = ('id', 'title', 'body')
